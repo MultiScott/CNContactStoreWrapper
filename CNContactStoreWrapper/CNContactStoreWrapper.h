fe78725805e9508a8626c07437e155be93975cce
MultiScott <scott@multion.ai>
2024-12-05 16:00:49 -0800

    Initial Commit

---
+++ CNContactStoreWrapper/CNContactStoreWrapper.h
@@ -0,0 +1,12 @@
+//
+//  CNContactStoreWrapper.h
+//  CNContactStoreWrapper
+//
+//  Created by Scott Hoge on 12/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CNContactStoreWrapper : NSObject
+
+@end
---
+++ CNContactStoreWrapper/CNContactStoreWrapper.m
@@ -0,0 +1,12 @@
+//
+//  CNContactStoreWrapper.m
+//  CNContactStoreWrapper
+//
+//  Created by Scott Hoge on 12/5/24.
+//
+
+#import "CNContactStoreWrapper.h"
+
+@implementation CNContactStoreWrapper
+
+@end
