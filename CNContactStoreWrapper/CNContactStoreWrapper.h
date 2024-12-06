650dc867ef4bb2c1fa569686044d1ee0eff713f0
MultiScott <scott@multion.ai>
2024-12-06 09:57:35 -0800

    Move docs to header.

--- CNContactStoreWrapper/CNContactStoreWrapper.h
+++ CNContactStoreWrapper/CNContactStoreWrapper.h
@@ -24,8 +24,37 @@ NS_ASSUME_NONNULL_BEGIN
 /// - Note: For more details on the Contacts framework, refer to the [Apple Documentation](https://developer.apple.com/documentation/contacts).
 @interface CNContactStoreWrapper : NSObject
 
+/// Initializes the wrapper with a specific `CNContactStore` instance.
+///
+/// - Parameter store: A `CNContactStore` instance to be used by this wrapper.
+///
+/// - Note: For more information, see [CNContactStore](https://developer.apple.com/documentation/contacts/cncontactstore).
 - (instancetype)initWithStore:(CNContactStore *)store NS_DESIGNATED_INITIALIZER;
 
+
+/// This method calls the Objective-C exclusive method `enumeratorForChangeHistoryFetchRequest`.
+///
+/// - Parameter request: A `CNChangeHistoryFetchRequest` object that describes the events to fetch.
+/// - Parameter error: If the fetch fails, contains an `NSError` object with more information.
+/// - Returns: A `CNFetchResult` object, which is returned by calling `enumeratorForChangeHistoryFetchRequest` on `CNContactStore`.
+///
+/// - Note: For more information, see [enumeratorForChangeHistoryFetchRequest](https://developer.apple.com/documentation/contacts/cncontactstore/enumeratorforchangehistoryfetchrequest:error:/)
+///  and [CNFetchResult](https://developer.apple.com/documentation/contacts/cnfetchresult)
+///
+/// Example:
+/// ```swift
+/// let result = wrapper.changeHistoryFetchResult(fetchRequest, error: nil)
+/// guard let enumerator = result.value as? NSEnumerator else {
+///     throw ContactError.failedToCreateNSEnumeratorForChangeHistory
+/// }
+/// enumerator
+///    .compactMap {
+///        $0 as? CNChangeHistoryEvent
+///    }
+///    .forEach { event in
+///        // Process each event
+///    }
+/// ```
 - (CNFetchResult *)changeHistoryFetchResult:(CNChangeHistoryFetchRequest *)request
                                       error:(NSError *__autoreleasing  _Nullable * _Nullable)error;
 
